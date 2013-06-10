b53285ac3cce9cb78f75a048a63fe940e28d628d
pivolan <pivolan@gmail.com>
2013-06-10 18:54:49 +0400

    перемещение файлов статики.

---
+++ web/js/6c11614af866f332d09710c5891c9aa1.js
@@ -0,0 +1,8 @@
+
+if(navigator.platform=="Win32"&&navigator.appName=="Microsoft Internet Explorer"&&window.attachEvent){window.attachEvent("onload",enableAlphaImages);}
+function enableAlphaImages(){var rslt=navigator.appVersion.match(/MSIE (\d+\.\d+)/,'');var itsAllGood=(rslt!=null&&Number(rslt[1])>=5.5);if(itsAllGood){for(var i=0;i<document.all.length;i++){var obj=document.all[i];var bg=obj.currentStyle.backgroundImage;var img=document.images[i];if(bg&&bg.match(/\.png/i)!=null){var img=bg.substring(5,bg.length-2);var offset=obj.style["background-position"];obj.style.filter="progid:DXImageTransform.Microsoft.AlphaImageLoader(src='"+img+"', sizingMethod='crop')";obj.style.backgroundImage="url('"+BLANK_IMG+"')";obj.style["background-position"]=offset;}else if(img&&img.src.match(/\.png$/i)!=null){var src=img.src;img.style.width=img.width+"px";img.style.height=img.height+"px";img.style.filter="progid:DXImageTransform.Microsoft.AlphaImageLoader(src='"+src+"', sizingMethod='crop')"
+img.src=BLANK_IMG;}}}};
+ieHover=function(){var items,iframe;items=$$('#nav ul','#nav div','.truncated_full_value .item-options','.tool-tip','#topCartContent');$$('#checkout-step-payment','.tool-tip','#topCartContent').each(function(el){el.show();el.setStyle({'visibility':'hidden'})})
+for(var j=0;j<items.length;j++){iframe=document.createElement('IFRAME');iframe.src=BLANK_URL;iframe.scrolling='no';iframe.frameBorder=0;iframe.className='hover-fix';iframe.style.width=items[j].offsetWidth+"px";iframe.style.height=items[j].offsetHeight+"px";items[j].insertBefore(iframe,items[j].firstChild);}
+$$('.tool-tip','#checkout-step-payment','#topCartContent').each(function(el){el.hide();el.setStyle({'visibility':'visible'})})}
+Event.observe(window,'load',ieHover);;
