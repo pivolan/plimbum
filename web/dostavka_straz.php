d9f16eb9ac4c1ad6b598662884330e81871b342d
pivolan <pivolan@gmail.com>
2013-06-14 13:03:33 +0400

    добавлены новые страницы для страз.

---
+++ web/dostavka_straz.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: PiVo
+ * Date: 03.07.12
+ * Time: 0:17
+ * To change this template use File | Settings | File Templates.
+ */
+require_once dirname(__FILE__) . '/../Twig/Autoloader.php';
+Twig_Autoloader::register();
+$loader = new Twig_Loader_Filesystem('../templates');
+$twig = new Twig_Environment($loader, array(
+    'debug' => true
+));
+$filename = str_replace('.php', '', basename(__FILE__));
+echo $twig->render("pages/$filename.html.twig");
